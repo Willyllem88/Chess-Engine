394dcf40b9864450a4dadd5d2d5661a53fb4e0b8
Willyllem88 <guillemcabre2004@gmail.com>
2024-05-17 11:12:04 +0200

    Removing some files

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include "Board.hh"
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 640;
+
+int main(int argc, char* argv[]) {
+    if (argc != 0) {
+        //MISSING: do a propper usage() function, do it propperly
+        printf("Usage: %s\n", argv[0]);
+        return 1;
+    }
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Chess Board", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr) {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    Board board(renderer);
+    if (!board.loadMedia()) {
+        printf("Failed to load media!\n");
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    bool quit = false;
+    SDL_Event e;
+
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderClear(renderer);
+        board.render();
+        SDL_RenderPresent(renderer);
+    }
+
+    board.free();
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    while(1);
+    return 0;
+}
