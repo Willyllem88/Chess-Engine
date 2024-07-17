123e10dac32c05b6374b86f285b46dcf1ed37b6e
Willyllem88 <guillemcabre2004@gmail.com>
2024-07-17 11:32:11 +0200

    Updated the gitignore

---
+++ build/CMakeFiles/engine.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engine.dir/src/board.cpp.o"
+  "CMakeFiles/engine.dir/src/board.cpp.o.d"
+  "CMakeFiles/engine.dir/src/game.cpp.o"
+  "CMakeFiles/engine.dir/src/game.cpp.o.d"
+  "CMakeFiles/engine.dir/src/legalMoves.cpp.o"
+  "CMakeFiles/engine.dir/src/legalMoves.cpp.o.d"
+  "CMakeFiles/engine.dir/src/main.cpp.o"
+  "CMakeFiles/engine.dir/src/main.cpp.o.d"
+  "CMakeFiles/engine.dir/src/myApp.cpp.o"
+  "CMakeFiles/engine.dir/src/myApp.cpp.o.d"
+  "CMakeFiles/engine.dir/src/players.cpp.o"
+  "CMakeFiles/engine.dir/src/players.cpp.o.d"
+  "CMakeFiles/engine.dir/src/utils.cpp.o"
+  "CMakeFiles/engine.dir/src/utils.cpp.o.d"
+  "engine"
+  "engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
