0a94712196aee42664b77c93d277070bcd2bf2fa
Willyllem88 <guillemcabre2004@gmail.com>
2024-07-17 10:51:07 +0200

    Introduced the use of cmake, it has portability benefits over make.

---
+++ build/CMakeFiles/engine.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/guill/OneDrive/Projectes/Chess-Engine/src/board.cpp" "CMakeFiles/engine.dir/src/board.cpp.o" "gcc" "CMakeFiles/engine.dir/src/board.cpp.o.d"
+  "/mnt/c/Users/guill/OneDrive/Projectes/Chess-Engine/src/game.cpp" "CMakeFiles/engine.dir/src/game.cpp.o" "gcc" "CMakeFiles/engine.dir/src/game.cpp.o.d"
+  "/mnt/c/Users/guill/OneDrive/Projectes/Chess-Engine/src/legalMoves.cpp" "CMakeFiles/engine.dir/src/legalMoves.cpp.o" "gcc" "CMakeFiles/engine.dir/src/legalMoves.cpp.o.d"
+  "/mnt/c/Users/guill/OneDrive/Projectes/Chess-Engine/src/main.cpp" "CMakeFiles/engine.dir/src/main.cpp.o" "gcc" "CMakeFiles/engine.dir/src/main.cpp.o.d"
+  "/mnt/c/Users/guill/OneDrive/Projectes/Chess-Engine/src/myApp.cpp" "CMakeFiles/engine.dir/src/myApp.cpp.o" "gcc" "CMakeFiles/engine.dir/src/myApp.cpp.o.d"
+  "/mnt/c/Users/guill/OneDrive/Projectes/Chess-Engine/src/players.cpp" "CMakeFiles/engine.dir/src/players.cpp.o" "gcc" "CMakeFiles/engine.dir/src/players.cpp.o.d"
+  "/mnt/c/Users/guill/OneDrive/Projectes/Chess-Engine/src/utils.cpp" "CMakeFiles/engine.dir/src/utils.cpp.o" "gcc" "CMakeFiles/engine.dir/src/utils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
